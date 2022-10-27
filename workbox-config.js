c63c40ec7150c36de97189b0555810c2b4a48970
EstebanOlea <114716953+EstebanOlea@users.noreply.github.com>
2022-10-27 09:04:17 -0500

    Initial commit

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'C:\Users\esteb\OneDrive\Escritorio\paginaMatematicas',
+	globPatterns: [
+		'**/*.{css,jpg,html,js,json}'
+	],
+	swDest: 'C:\Users\esteb\OneDrive\Escritorio\paginaMatematicas/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
